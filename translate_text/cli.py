44074c83dc04b7da9661f377da94c31f85825f32
pavlosprotopapas <protopapas@gmail.com>
2024-09-15 23:13:11 -0400

    fixed translate-text

--- translate_text/cli.py
+++ translate_text/cli.py
@@ -15,7 +15,7 @@ gcp_project = "ac215-project"
 bucket_name = "mega-pipeline-bucket"
 text_paragraphs = "text_paragraphs"
 text_translated = "text_translated"
-group_name = "pavlos" # This needs to be your Group name e.g: group-01, group-02, group-03, group-04, group-05, ...
+group_name = "pavlos-advanced" # This needs to be your Group name e.g: group-01, group-02, group-03, group-04, group-05, ...
 
 translator = Translator()
 
