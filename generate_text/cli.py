95cf6f1769316efb28d024924ae4c6a7eb86e099
pavlosprotopapas <protopapas@gmail.com>
2026-09-15 08:43:46 -0400

    flexible corrected with right secrets

--- generate_text/cli.py
+++ generate_text/cli.py
@@ -13,11 +13,11 @@ from google import genai
 # Generate the inputs arguments parser
 parser = argparse.ArgumentParser(description="Command description.")
 
-gcp_project = "" #"ac215-project"
-bucket_name = ""  #"mega-pipeline-bucket"
+gcp_project = "ac215-project"
+bucket_name = "mega-pipeline-bucket"
 text_prompts = "text_prompts"  # THIS IS THE TRANSCRIBED TEXT
 text_paragraphs = "text_paragraphs"  # THIS IS THE LLM GENERATED TEXT
-group_name = "" #"pavlos-advanced"
+group_name = "pavlos-advanced"  # This needs to be your Group name e.g: group-01, group-02, group-03, group-04, group-05, ...
 assert group_name != "", "Update group name"
 #assert group_name != "pavlos-advanced", "Update group name"
 #############################################################################
